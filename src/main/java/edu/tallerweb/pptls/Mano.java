21fce9bc3fd063794098d099e834191049b887fe
jorge <mirandajorgeruben@gmail.com>
2013-10-02 19:27:06 -0300

    Version final final de final

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -1,27 +1,42 @@
 package edu.tallerweb.pptls;
 
+import java.util.ArrayList;
+
 /**
  * Representa una de las Manos involucradas en el juego
  */
 public class Mano {
-
+	private final Forma forma;
+	private final ArrayList<Integer> perdioCon = new ArrayList<Integer>();
 	/**
-	 * Toda Mano debe crearse con una forma dada, que será
-	 * la que determine su condición en el juego.
+	 * Toda Mano debe crearse con una forma dada, que ser�
+	 * la que determine su condici�n en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		this.forma=forma;
+		this.perdioCon.add(getNext(0));
+		this.perdioCon.add(getNext(1));
 	}
-
 	/**
-	 * Evaluará el resultado de la partida según las reglas
-	 * del juego.
-	 * @param otra, la otra Mano.
-	 * @return un Resultado, de acuerdo al estado del juego.
+	 se reservan para evaluar estados del Resultado.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		if(this.forma.getValor().equals(otra.forma.getValor()))
+			return Resultado.EMPATA;
+		if(this.perdioCon.contains(otra.forma.getValor()))
+			return Resultado.GANA;
+		return Resultado.PIERDE;
+	}
+/*
+ * es necesario que al recorrerlo compare con los proximos 2, 
+ * ya que a los demas les gana y con simismo empata.
+ * */
+	private Integer getNext(int out){
+		if(this.forma.getValor()-out>4){
+			return (this.forma.getValor()-out-5);		
+		}
+		return this.forma.getValor()-out;
 	}
 
 }
